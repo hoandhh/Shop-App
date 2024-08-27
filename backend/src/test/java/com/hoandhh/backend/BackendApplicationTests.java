43d7b73e2b063180bad856a81537569a8c96141f
Hoan Doan <hoandoan288@gmail.com>
2024-08-27 15:24:03 +0700

    Restructure prj

---
+++ backend/src/test/java/com/hoandhh/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoandhh.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
--- be/temp.java
+++
@@ -1,1 +0,0 @@
-// Code backend here
